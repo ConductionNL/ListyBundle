c48bce9e6c4f6b5126d5cef88c0d75474d97789d
Ruben van der Linde <ruben@conduction.nl>
2020-06-27 23:22:37 +0200

    First Code

---
+++ ListyBundle.php
@@ -0,0 +1,20 @@
+<?php
+
+// Conduction/ListyBundle/ListyBundle.php
+
+/*
+ * This file is part of the Conduction Listy
+ *
+ * (c) Conduction <info@conduction.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Conduction\ListyBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ListyBundle extends Bundle
+{
+}

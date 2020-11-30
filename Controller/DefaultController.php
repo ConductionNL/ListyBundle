0a92cd9a3231a37dc9404101e3d2fadefca2834e
ginokok1996 <ginokok1996@hotmail.com>
2020-11-30 15:20:12 +0100

    5.1 fixes

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -5,7 +5,6 @@
 namespace Conduction\ListyBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\Session;
@@ -18,7 +17,7 @@ use Symfony\Component\Routing\Annotation\Route;
  *
  * @Route("/")
  */
-class DefaultController extends AbstractController
+class DefaultController
 {
     /**
      * This function serves the base (home) path for this template

3770146b96720e46750bd6b7d13cc3a9253997ed
ginokok1996 <ginokok1996@hotmail.com>
2020-11-30 12:58:32 +0100

    ListlyBundle update

---
+++ Controller/DefaultController.php
@@ -0,0 +1,36 @@
+<?php
+
+// Controller/DefaultController.php
+
+namespace Conduction\ListlyBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * The index test handles any calls that have not been picked up by another test, and wel try to handle the slug based against the wrc.
+ *
+ * Class DefaultController
+ *
+ * @Route("/")
+ */
+class DefaultController extends AbstractController
+{
+    /**
+     * This function serves the base (home) path for this template
+     *
+     * "/", name="conduction_listly_default_index", priority=2
+     * @Route("/", name="conduction_listly_default_index")
+     * @Template
+     */
+    public function indexAction(Session $session, Request $request)
+    {
+        $variable = [];
+
+        return $variable;
+    }
+}
